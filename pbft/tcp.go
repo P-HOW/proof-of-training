9a379695f8b0cc189cc9a70630d82b474f83c1d7
P-How <peihaoli1014@gmail.com>
2023-06-05 15:32:52 +0800

    updated pbft utilities

--- pbft/tcp.go
+++ pbft/tcp.go
@@ -1,136 +1,68 @@
 package pbft
 
 import (
-	"crypto"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
-	"strconv"
+	"net"
 )
 
-// 如果当前目录下不存在目录Keys，则创建目录，并为各个节点生成rsa公私钥
-func genRsaKeys() {
-	if !isExist("./Keys") {
-		fmt.Println("检测到还未生成公私钥目录，正在生成公私钥 ...")
-		err := os.Mkdir("Keys", 0644)
+// 客户端使用的tcp监听
+func clientTcpListen() {
+	listen, err := net.Listen("tcp", clientAddr)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer listen.Close()
+
+	for {
+		conn, err := listen.Accept()
 		if err != nil {
-			log.Panic()
+			log.Panic(err)
 		}
-		for i := 0; i <= 4; i++ {
-			if !isExist("./Keys/N" + strconv.Itoa(i)) {
-				err := os.Mkdir("./Keys/N"+strconv.Itoa(i), 0644)
-				if err != nil {
-					log.Panic()
-				}
-			}
-			priv, pub := getKeyPair()
-			privFileName := "Keys/N" + strconv.Itoa(i) + "/N" + strconv.Itoa(i) + "_RSA_PIV"
-			file, err := os.OpenFile(privFileName, os.O_RDWR|os.O_CREATE, 0644)
-			if err != nil {
-				log.Panic(err)
-			}
-			defer file.Close()
-			file.Write(priv)
-
-			pubFileName := "Keys/N" + strconv.Itoa(i) + "/N" + strconv.Itoa(i) + "_RSA_PUB"
-			file2, err := os.OpenFile(pubFileName, os.O_RDWR|os.O_CREATE, 0644)
-			if err != nil {
-				log.Panic(err)
-			}
-			defer file2.Close()
-			file2.Write(pub)
+		b, err := ioutil.ReadAll(conn)
+		if err != nil {
+			log.Panic(err)
 		}
-		fmt.Println("已为节点们生成RSA公私钥")
+		fmt.Println(string(b))
 	}
+
 }
 
-// 生成rsa公私钥
-func getKeyPair() (prvkey, pubkey []byte) {
-	// 生成私钥文件
-	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+// 节点使用的tcp监听
+func (p *pbft) tcpListen() {
+	listen, err := net.Listen("tcp", p.node.addr)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
-	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
-	block := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: derStream,
-	}
-	prvkey = pem.EncodeToMemory(block)
-	publicKey := &privateKey.PublicKey
-	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
-	if err != nil {
-		panic(err)
-	}
-	block = &pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: derPkix,
-	}
-	pubkey = pem.EncodeToMemory(block)
-	return
-}
+	fmt.Printf("节点开启监听，地址：%s\n", p.node.addr)
+	defer listen.Close()
 
-// 判断文件或文件夹是否存在
-func isExist(path string) bool {
-	_, err := os.Stat(path)
-	if err != nil {
-		if os.IsExist(err) {
-			return true
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Panic(err)
 		}
-		if os.IsNotExist(err) {
-			return false
+		b, err := ioutil.ReadAll(conn)
+		if err != nil {
+			log.Panic(err)
 		}
-		fmt.Println(err)
-		return false
+		p.handleRequest(b)
 	}
-	return true
+
 }
 
-// 数字签名
-func (p *pbft) RsaSignWithSha256(data []byte, keyBytes []byte) []byte {
-	h := sha256.New()
-	h.Write(data)
-	hashed := h.Sum(nil)
-	block, _ := pem.Decode(keyBytes)
-	if block == nil {
-		panic(errors.New("private key error"))
-	}
-	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+// 使用tcp发送消息
+func tcpDial(context []byte, addr string) {
+	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		fmt.Println("ParsePKCS8PrivateKey err", err)
-		panic(err)
+		log.Println("connect error", err)
+		return
 	}
 
-	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed)
+	_, err = conn.Write(context)
 	if err != nil {
-		fmt.Printf("Error from signing: %s\n", err)
-		panic(err)
+		log.Fatal(err)
 	}
-
-	return signature
-}
-
-// 签名验证
-func (p *pbft) RsaVerySignWithSha256(data, signData, keyBytes []byte) bool {
-	block, _ := pem.Decode(keyBytes)
-	if block == nil {
-		panic(errors.New("public key error"))
-	}
-	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		panic(err)
-	}
-
-	hashed := sha256.Sum256(data)
-	err = rsa.VerifyPKCS1v15(pubKey.(*rsa.PublicKey), crypto.SHA256, hashed[:], signData)
-	if err != nil {
-		panic(err)
-	}
-	return true
+	conn.Close()
 }
