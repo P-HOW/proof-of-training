13f178bf0944ebcbf251452044fb753cf09cafcc
P-How <peihaoli1014@gmail.com>
2023-06-05 23:38:55 +0800

    update pbft function forms

--- pbft/pbft.go
+++ pbft/pbft.go
@@ -10,41 +10,48 @@ import (
 	"sync"
 )
 
-// 本地消息池（模拟持久化层），只有确认提交成功后才会存入此池
+// Local message pool (simulating the persistence layer), only after the confirmation of successful commit will the messages be stored in this pool.
 var localMessagePool = []Message{}
 
 // Node table for broadcasting
 type nodeTable map[string]string
 
 type node struct {
-	//节点ID
+	//Node ID
 	nodeID string
-	//节点监听地址
+	//Node Listening Address
 	addr string
-	//RSA私钥
+	//RSA private key
 	rsaPrivKey []byte
-	//RSA公钥
+	//RSA public key
 	rsaPubKey []byte
 }
 
 type pbft struct {
-	//节点信息
+	//node information
 	node node
-	//每笔请求自增序号
+	//Each request increases the sequence number.
 	sequenceID int
-	//锁
+	//lock
 	lock sync.Mutex
-	//临时消息池，消息摘要对应消息本体
+	//
+	//Temporary message pool, the message digest corresponds to the message body.
 	messagePool map[string]Request
-	//存放收到的prepare数量(至少需要收到并确认2f个)，根据摘要来对应
+	//
+	//The number of prepares received (at least 2f are needed to be received and confirmed), corresponding according to the digest.
 	prePareConfirmCount map[string]map[string]bool
-	//存放收到的commit数量（至少需要收到并确认2f+1个），根据摘要来对应
+	//
+	//Stores the number of commits received (at least 2f+1 are needed to be received and confirmed), corresponding according to the digest.
 	commitConfirmCount map[string]map[string]bool
-	//该笔消息是否已进行Commit广播
+	//
+	//Has the broadcast for this message already been committed
 	isCommitBordcast map[string]bool
-	//该笔消息是否已对客户端进行Reply
-	isReply   map[string]bool
+	//
+	//Has this message already been replied to the client?
+	isReply map[string]bool
+
 	nodeTable nodeTable
+
 	nodeCount int
 }
 
@@ -52,8 +59,8 @@ func NewPBFT(nodeID, addr string, nodeCount int) *pbft {
 	p := new(pbft)
 	p.node.nodeID = nodeID
 	p.node.addr = addr
-	p.node.rsaPrivKey = p.getPivKey(nodeID) //从生成的私钥文件处读取
-	p.node.rsaPubKey = p.getPubKey(nodeID)  //从生成的私钥文件处读取
+	p.node.rsaPrivKey = p.getPivKey(nodeID) //Read from the generated private key file.
+	p.node.rsaPubKey = p.getPubKey(nodeID)  //Read from the generated private key file.
 	p.sequenceID = 0
 	p.messagePool = make(map[string]Request)
 	p.prePareConfirmCount = make(map[string]map[string]bool)
@@ -66,7 +73,7 @@ func NewPBFT(nodeID, addr string, nodeCount int) *pbft {
 }
 
 func (p *pbft) handleRequest(data []byte) {
-	//切割消息，根据消息命令调用不同的功能
+	//Split the message and call different functions based on the message command.
 	cmd, content := splitMessage(data)
 	switch command(cmd) {
 	case cRequest:
@@ -80,178 +87,181 @@ func (p *pbft) handleRequest(data []byte) {
 	}
 }
 
-// 处理客户端发来的请求
+// Handle requests coming from the client.
 func (p *pbft) handleClientRequest(content []byte) {
-	fmt.Println("主节点已接收到客户端发来的request ...")
-	//使用json解析出Request结构体
+	fmt.Println("The primary node has received a request from the client...")
+	//Parsing the Request structure using JSON.
 	r := new(Request)
 	err := json.Unmarshal(content, r)
 	if err != nil {
 		log.Panic(err)
 	}
-	//添加信息序号
+	//add sequence number
 	p.sequenceIDAdd()
-	//获取消息摘要
+	//fetch digest
 	digest := getDigest(*r)
-	fmt.Println("已将request存入临时消息池")
-	//存入临时消息池
+	fmt.Println("The request has been stored in the temporary message pool.")
+	//Store in the temporary message pool.
 	p.messagePool[digest] = *r
-	//主节点对消息摘要进行签名
+	//The primary node signs the message digest.
 	digestByte, _ := hex.DecodeString(digest)
 	signInfo := p.RsaSignWithSha256(digestByte, p.node.rsaPrivKey)
-	//拼接成PrePrepare，准备发往follower节点
+	//Assembled into PrePrepare, ready to be sent to follower nodes.
 	pp := PrePrepare{*r, digest, p.sequenceID, signInfo}
 	b, err := json.Marshal(pp)
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println("正在向其他节点进行进行PrePrepare广播 ...")
-	//进行PrePrepare广播
+	fmt.Println("Broadcasting PrePrepare to other nodes...")
+	//Broadcast PrePrepare
 	p.broadcast(cPrePrepare, b)
-	fmt.Println("PrePrepare广播完成")
+	fmt.Println("PrePrepare broadcast completed.")
 }
 
-// 处理预准备消息
+// Process PrePrepare message
 func (p *pbft) handlePrePrepare(content []byte) {
-	fmt.Println("本节点已接收到主节点发来的PrePrepare ...")
-	//	//使用json解析出PrePrepare结构体
+	fmt.Println("This node has received the PrePrepare message sent by the primary node ...")
+	//Parse out the PrePrepare structure using JSON
 	pp := new(PrePrepare)
 	err := json.Unmarshal(content, pp)
 	if err != nil {
 		log.Panic(err)
 	}
-	//获取主节点的公钥，用于数字签名验证
+	//To obtain the public key of the primary node for digital signature verification
 	primaryNodePubKey := p.getPubKey("N0")
 	digestByte, _ := hex.DecodeString(pp.Digest)
 	if digest := getDigest(pp.RequestMessage); digest != pp.Digest {
-		fmt.Println("信息摘要对不上，拒绝进行prepare广播")
+		fmt.Println("The digest doesn't match, refuse to broadcast prepare")
 	} else if p.sequenceID+1 != pp.SequenceID {
-		fmt.Println("消息序号对不上，拒绝进行prepare广播")
+		fmt.Println("The message sequence number doesn't match, refuse to broadcast prepare")
 	} else if !p.RsaVerySignWithSha256(digestByte, pp.Sign, primaryNodePubKey) {
-		fmt.Println("主节点签名验证失败！,拒绝进行prepare广播")
+		fmt.Println("The primary node signature verification failed! Refusing to broadcast prepare")
 	} else {
-		//序号赋值
+		//Assigning the sequence number
 		p.sequenceID = pp.SequenceID
-		//将信息存入临时消息池
-		fmt.Println("已将消息存入临时节点池")
+		//Storing the information in the temporary message pool
+		fmt.Println("The message has been stored in the temporary node pool")
 		p.messagePool[pp.Digest] = pp.RequestMessage
-		//节点使用私钥对其签名
+		//The node signs it with its private key
 		sign := p.RsaSignWithSha256(digestByte, p.node.rsaPrivKey)
-		//拼接成Prepare
+		//Concatenate to form a Prepare message
 		pre := Prepare{pp.Digest, pp.SequenceID, p.node.nodeID, sign}
 		bPre, err := json.Marshal(pre)
 		if err != nil {
 			log.Panic(err)
 		}
 		//进行准备阶段的广播
-		fmt.Println("正在进行Prepare广播 ...")
+		fmt.Println("broadcasting the Prepare message...")
 		p.broadcast(cPrepare, bPre)
-		fmt.Println("Prepare广播完成")
+		fmt.Println("Prepare broadcast is completed.")
 	}
 }
 
-// 处理准备消息
+// Process the Prepare message
 func (p *pbft) handlePrepare(content []byte, nodeCount int) {
-	//使用json解析出Prepare结构体
+	//Parse out the Prepare structure using JSON
 	pre := new(Prepare)
 	err := json.Unmarshal(content, pre)
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Printf("本节点已接收到%s节点发来的Prepare ... \n", pre.NodeID)
-	//获取消息源节点的公钥，用于数字签名验证
+	fmt.Printf("The node has received Prepare from node %s ... \n", pre.NodeID)
+	//To obtain the public key of the message source node for digital signature verification
 	MessageNodePubKey := p.getPubKey(pre.NodeID)
 	digestByte, _ := hex.DecodeString(pre.Digest)
 	if _, ok := p.messagePool[pre.Digest]; !ok {
-		fmt.Println("当前临时消息池无此摘要，拒绝执行commit广播")
+		fmt.Println("The current temporary message pool does not have this digest. Refusing to execute commit broadcast")
 	} else if p.sequenceID != pre.SequenceID {
-		fmt.Println("消息序号对不上，拒绝执行commit广播")
+		fmt.Println("The message sequence number doesn't match. Refusing to execute commit broadcast")
 	} else if !p.RsaVerySignWithSha256(digestByte, pre.Sign, MessageNodePubKey) {
-		fmt.Println("节点签名验证失败！,拒绝执行commit广播")
+		fmt.Println("The node signature verification failed! Refusing to execute commit broadcast")
 	} else {
 		p.setPrePareConfirmMap(pre.Digest, pre.NodeID, true)
 		count := 0
 		for range p.prePareConfirmCount[pre.Digest] {
 			count++
 		}
-		//因为主节点不会发送Prepare，所以不包含自己
+		//Because the primary node does not send Prepare, it does not include itself
 		specifiedCount := 0
 		if p.node.nodeID == "N0" {
 			specifiedCount = nodeCount / 3 * 2
 		} else {
 			specifiedCount = (nodeCount / 3 * 2) - 1
 		}
-		//如果节点至少收到了2f个prepare的消息（包括自己）,并且没有进行过commit广播，则进行commit广播
+		//If a node has received at least 2f Prepare messages (including itself) and
+		//has not yet performed a commit broadcast, it will proceed with a commit broadcast
 		p.lock.Lock()
-		//获取消息源节点的公钥，用于数字签名验证
+		//To obtain the public key of the message source node for digital signature verification
 		if count >= specifiedCount && !p.isCommitBordcast[pre.Digest] {
-			fmt.Println("本节点已收到至少2f个节点(包括本地节点)发来的Prepare信息 ...")
-			//节点使用私钥对其签名
+			fmt.Println("This node has received at least 2f Prepare messages (including the local node) from other nodes ...")
+			//The node signs it with its private key
 			sign := p.RsaSignWithSha256(digestByte, p.node.rsaPrivKey)
 			c := Commit{pre.Digest, pre.SequenceID, p.node.nodeID, sign}
 			bc, err := json.Marshal(c)
 			if err != nil {
 				log.Panic(err)
 			}
-			//进行提交信息的广播
-			fmt.Println("正在进行commit广播")
+			//Broadcasting the commit message
+			fmt.Println("broadcasting the commit message...")
 			p.broadcast(cCommit, bc)
 			p.isCommitBordcast[pre.Digest] = true
-			fmt.Println("commit广播完成")
+			fmt.Println("commit broadcast is completed")
 		}
 		p.lock.Unlock()
 	}
 }
 
-// 处理提交确认消息
+// Processing the commit
 func (p *pbft) handleCommit(content []byte, nodeCount int) {
-	//使用json解析出Commit结构体
+	//Parse out the Commit structure using JSON
 	c := new(Commit)
 	err := json.Unmarshal(content, c)
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Printf("本节点已接收到%s节点发来的Commit ... \n", c.NodeID)
-	//获取消息源节点的公钥，用于数字签名验证
+	fmt.Printf("The node has received Commit from node %s ... \n", c.NodeID)
+	//To obtain the public key of the message source node for digital signature verification
 	MessageNodePubKey := p.getPubKey(c.NodeID)
 	digestByte, _ := hex.DecodeString(c.Digest)
 	if _, ok := p.prePareConfirmCount[c.Digest]; !ok {
-		fmt.Println("当前prepare池无此摘要，拒绝将信息持久化到本地消息池")
+		fmt.Println("The current Prepare pool does not have this digest. Refusing to persist the information to the local message pool")
 	} else if p.sequenceID != c.SequenceID {
-		fmt.Println("消息序号对不上，拒绝将信息持久化到本地消息池")
+		fmt.Println("The message sequence number doesn't match. Refusing to persist the information to the local message pool")
 	} else if !p.RsaVerySignWithSha256(digestByte, c.Sign, MessageNodePubKey) {
-		fmt.Println("节点签名验证失败！,拒绝将信息持久化到本地消息池")
+		fmt.Println("The node signature verification failed! Refusing to persist the information to the local message pool")
 	} else {
 		p.setCommitConfirmMap(c.Digest, c.NodeID, true)
 		count := 0
 		for range p.commitConfirmCount[c.Digest] {
 			count++
 		}
-		//如果节点至少收到了2f+1个commit消息（包括自己）,并且节点没有回复过,并且已进行过commit广播，则提交信息至本地消息池，并reply成功标志至客户端！
+		//If a node has received at least 2f+1 commit messages (including itself), and the node has not replied before,
+		//and a commit broadcast has been performed, then the information is submitted to the local message pool,
+		//and a successful flag is replied to the client!
 		p.lock.Lock()
 		if count >= nodeCount/3*2 && !p.isReply[c.Digest] && p.isCommitBordcast[c.Digest] {
-			fmt.Println("本节点已收到至少2f + 1 个节点(包括本地节点)发来的Commit信息 ...")
-			//将消息信息，提交到本地消息池中！
+			fmt.Println("This node has received at least 2f + 1 Commit messages (including the local node) from other nodes ...")
+			//The message information is being submitted to the local message pool!
 			localMessagePool = append(localMessagePool, p.messagePool[c.Digest].Message)
-			info := p.node.nodeID + "节点已将msgid:" + strconv.Itoa(p.messagePool[c.Digest].ID) + "存入本地消息池中,消息内容为：" + p.messagePool[c.Digest].Content
+			info := p.node.nodeID + "node has put msgid:" + strconv.Itoa(p.messagePool[c.Digest].ID) + "into the local message pool,message content：" + p.messagePool[c.Digest].Content
 			fmt.Println(info)
-			fmt.Println("正在reply客户端 ...")
+			fmt.Println("Replying to client ...")
 			tcpDial([]byte(info), p.messagePool[c.Digest].ClientAddr)
 			p.isReply[c.Digest] = true
-			fmt.Println("reply完毕")
+			fmt.Println("replying done!")
 		}
 		p.lock.Unlock()
 	}
 }
 
-// 序号累加
+// Add sequenceID
 func (p *pbft) sequenceIDAdd() {
 	p.lock.Lock()
 	p.sequenceID++
 	p.lock.Unlock()
 }
 
-// 向除自己外的其他节点进行广播
+// Broadcasting to other nodes except itself
 func (p *pbft) broadcast(cmd command, content []byte) {
 	for i := range p.nodeTable {
 		if i == p.node.nodeID {
@@ -262,7 +272,7 @@ func (p *pbft) broadcast(cmd command, content []byte) {
 	}
 }
 
-// 为多重映射开辟赋值
+// Allocating assignment for multiple mappings
 func (p *pbft) setPrePareConfirmMap(val, val2 string, b bool) {
 	if _, ok := p.prePareConfirmCount[val]; !ok {
 		p.prePareConfirmCount[val] = make(map[string]bool)
@@ -270,7 +280,7 @@ func (p *pbft) setPrePareConfirmMap(val, val2 string, b bool) {
 	p.prePareConfirmCount[val][val2] = b
 }
 
-// 为多重映射开辟赋值
+// Allocating assignment for multiple mappings
 func (p *pbft) setCommitConfirmMap(val, val2 string, b bool) {
 	if _, ok := p.commitConfirmCount[val]; !ok {
 		p.commitConfirmCount[val] = make(map[string]bool)
@@ -278,7 +288,7 @@ func (p *pbft) setCommitConfirmMap(val, val2 string, b bool) {
 	p.commitConfirmCount[val][val2] = b
 }
 
-// 传入节点编号， 获取对应的公钥
+// Pass the node number to obtain the corresponding public key
 func (p *pbft) getPubKey(nodeID string) []byte {
 	key, err := ioutil.ReadFile("Keys/" + nodeID + "/" + nodeID + "_RSA_PUB")
 	if err != nil {
@@ -287,7 +297,7 @@ func (p *pbft) getPubKey(nodeID string) []byte {
 	return key
 }
 
-// 传入节点编号， 获取对应的私钥
+// Pass the node number and obtain the corresponding private key
 func (p *pbft) getPivKey(nodeID string) []byte {
 	key, err := ioutil.ReadFile("Keys/" + nodeID + "/" + nodeID + "_RSA_PIV")
 	if err != nil {
