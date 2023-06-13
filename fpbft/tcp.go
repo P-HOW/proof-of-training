1313be655584a879802d467ad07e57076116a42f
P-How <peihaoli1014@gmail.com>
2023-06-13 16:06:16 +0800

    added tcpDial

---
+++ fpbft/tcp.go
@@ -0,0 +1,21 @@
+package pbft
+
+import (
+	"log"
+	"net"
+)
+
+// TCP send messages
+func tcpDial(context []byte, addr string) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Println("connect error", err)
+		return
+	}
+
+	_, err = conn.Write(context)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn.Close()
+}
