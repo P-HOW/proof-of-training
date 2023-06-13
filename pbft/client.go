a803240a498b55cb58d00ca3ed8fe3b522025d6a
P-How <peihaoli1014@gmail.com>
2023-06-13 15:21:07 +0800

    added client struct and utilities

--- pbft/client.go
+++ pbft/client.go
@@ -3,28 +3,34 @@ package pbft
 import (
 	"crypto/rand"
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"math/big"
+	"net"
 	"strings"
 	"sync"
 	"time"
 )
 
-func clientSendMessageAndListen(clientAddr string, nodeTable nodeTable, data string, numNodes int) float64 {
+type client struct {
+	clientAddr string
+	index      int //client ID for convenience purposes
+}
+
+func (c *client) clientSendMessageAndListen(nodeTable nodeTable, data string, numNodes int) float64 {
 	var wg sync.WaitGroup
 
 	//Start local monitoring of the client (mainly used to receive reply information from nodes).
 	go func() {
 		defer wg.Done()
-		clientTcpListen(clientAddr, numNodes)
+		c.clientTcpListen(numNodes)
 	}()
 
 	wg.Add(1) // Expect numNodes number of replies
 
 	r := new(Request)
 	r.Timestamp = time.Now().UnixNano()
-	r.ClientAddr = clientAddr
+	r.ClientAddr = c.clientAddr
 	r.Message.ID = getRandom()
 	//The message content is the user's input
 	r.Message.Content = strings.TrimSpace(data)
@@ -32,7 +38,7 @@ func clientSendMessageAndListen(clientAddr string, nodeTable nodeTable, data str
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(string(br))
+	//fmt.Println(string(br))
 	content := jointMessage(cRequest, br)
 	currentTime := time.Now()
 	//N0 is the primary node, and the request information is sent directly to N0 by default
@@ -44,7 +50,33 @@ func clientSendMessageAndListen(clientAddr string, nodeTable nodeTable, data str
 
 }
 
-// 返回一个十位数的随机数，作为msgid
+// TCP listening from clinet side
+func (c *client) clientTcpListen(numNodes int) {
+	listen, err := net.Listen("tcp", c.clientAddr)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer listen.Close()
+	count := 0
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		b, err := ioutil.ReadAll(conn)
+		if err != nil {
+			log.Panic(err)
+		}
+		//fmt.Println("client received" + string(b))
+		_ = b
+		count++
+		if count > numNodes/3*2 {
+			break
+		}
+	}
+}
+
+// Returns a ten-digit random number as msgid
 func getRandom() int {
 	x := big.NewInt(10000000000)
 	for {
